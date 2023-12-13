b25bb2f8a7094545cb2671a1d22f610d91da875a
Norberto Andres Luna <betoluna89@gmail.com>
2023-12-13 17:07:30 -0300

    Desktop view done

--- src/components/FraseCard.jsx
+++ src/components/FraseCard.jsx
@@ -2,24 +2,45 @@ export const FraseCard = () => {
   return (
     <div
       id="card"
-      className="text-center bg-darkGrayBlue font-manrope w-[90%] max-w-[400px] rounded-lg shadow-md relative"
+      className="text-center bg-darkGrayBlue font-manrope w-[90%] max-w-[575px] rounded-xl shadow-md relative"
     >
-      <div className="p-4">
-        <h2 className="text-neonGreen text-xs my-7 tracking-[3px]">
+      <div className="p-4 lg:px-10">
+        <h2 className="text-neonGreen text-xs my-7 md:my-8 tracking-[3px] md:text-sm">
           ADVICE #117
         </h2>
-        <p className="text-lightCyan text-2xl">
+        <p className="text-lightCyan text-2xl md:text-3xl md:leading-10">
           ¨It is easy to sit up and take notice, what`s difficult is getting up
-          and taking action¨
+          and taking action.¨
         </p>
-        <div className="flex gap-5 justify-evenly items-center mt-6 pb-10">
-          <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg">
+        <div className="flex gap-5 justify-evenly items-center mt-8 md:mt-12 pb-10 md:pb-14">
+          {/* Mobile */}
+          <svg
+            width="295"
+            height="16"
+            xmlns="http://www.w3.org/2000/svg"
+            className="md:hidden"
+          >
             <g fill="hsl(217, 19%, 38%)">
               <path
                 fill="hsl(217, 19%, 38%)"
                 d="M0 8h122v1H0zM173 8h122v1H173z"
               />
               <g transform="translate(138)" fill="#CEE3E9">
+                <rect width="6" height="16" rx="3" />
+                <rect x="14" width="6" height="16" rx="3" />
+              </g>
+            </g>
+          </svg>
+          {/* Desktop */}
+          <svg
+            width="444"
+            height="16"
+            xmlns="http://www.w3.org/2000/svg"
+            className="hidden md:block"
+          >
+            <g fill="none">
+              <path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z" />
+              <g transform="translate(212)" fill="#CEE3E9">
                 <rect width="6" height="16" rx="3" />
                 <rect x="14" width="6" height="16" rx="3" />
               </g>
